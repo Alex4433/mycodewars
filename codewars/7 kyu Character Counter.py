0d17141b5177b1cf6848dc0ff1ed478b448aeec8
Alex <a>
2022-02-08 07:48:12 +0300

    + solution

---
+++ codewars/7 kyu Character Counter.py
@@ -0,0 +1,34 @@
+def validate_word(word):
+    word = word.upper()
+    array = dict()
+    for letter in word:
+        if letter in array:
+            array[letter] += 1
+        else:
+            array[letter] = 1
+    len = None
+    for key in array:
+        if len is None:
+            len = array[key]
+            continue
+        else:
+            if len == array[key]:
+                continue
+            else:
+                return False
+    return True
+
+
+'''
+You are going to be given a word. Your job will be to make sure that each character in that word has the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+
+For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+
+The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. The length will be 0 < length < 100.
+Examples
+
+    "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+    "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
+    "123abc!" is a valid word because all of the characters only appear once in the word.
+
+'''
